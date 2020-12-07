e7c90b93262cbd52dd82ab1e4ab225b1979cf80c
Márton Kozma <kozmartinus@gmail.com>
2020-12-07 15:26:43 +0100

    add routes for App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Header from "./components/Header";
+
 import "./App.css";
 
 class App extends Component {
+  state = {
+    pokemons: [],
+    types: [],
+  };
+
   render() {
-    return <h2>Hello ssss</h2>;
+    return (
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route
+              exact
+              path="/pokemons"
+              render={(props) => <React.Fragment></React.Fragment>}
+            />
+            <Route
+              exact
+              path="/types"
+              render={(props) => <React.Fragment></React.Fragment>}
+            />
+            <Route exact path="/" component={Home} />
+          </div>
+        </div>
+      </Router>
+    );
   }
 }
 
