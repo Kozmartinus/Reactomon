3d2f3b67ca5bf9645a11ed71f1cda9cbf6e069b5
Márton Kozma <kozmartinus@gmail.com>
2020-12-09 22:15:11 +0100

    modify PokemonList to hook version

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import Pokemon from "./Pokemon";
 
-class PokemonList extends Component {
-  render() {
-    return (
-      <div>
-        {this.props.pokemons.map((pokemon) => (
-          <Pokemon pokemon={pokemon} />
-        ))}
-      </div>
-    );
-  }
-}
+const PokemonList = (props) => {
+  return (
+    <div>
+      {props.pokemons.map((pokemon) => (
+        <Pokemon pokemon={pokemon} />
+      ))}
+    </div>
+  );
+};
 
 export default PokemonList;
