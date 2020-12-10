1e8c4f54ae7b24e991bffed5f8c7db95a56d4e03
Márton Kozma <kozmartinus@gmail.com>
2020-12-10 19:12:48 +0100

    add Pagination styled component

---
+++ src/elements/PaginationElement.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const PaginationDiv = styled.div`
+  width: 160px;
+`;
