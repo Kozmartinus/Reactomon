5210d9c0f7c23739d71f596b204b42349e98d39a
Márton Kozma <kozmartinus@gmail.com>
2020-12-10 14:27:37 +0100

    add link for pokemon details on Pokemon List page

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Pokemon = (props) => {
   const { name, url } = props.pokemon;
+  const splittedUrl = url.split("/");
+  const id = splittedUrl[splittedUrl.length - 2];
   return (
     <div className="card">
       <p>{name}</p>
-      <p>{url}</p>
+      <Link to={`/pokemon/${id}`} pokemonId={id}>
+        See details
+      </Link>
     </div>
   );
 };
