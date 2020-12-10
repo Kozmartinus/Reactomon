cca74bb543e467636aa570b692e26a54d9dd81e7
Márton Kozma <kozmartinus@gmail.com>
2020-12-10 19:11:43 +0100

    add CardDiv styled component

---
+++ src/elements/CardDiv.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export default styled.div`
+  border: 2px solid #3b4cca;
+  background-color: #ffff56;
+  border-radius: 2em;
+  padding: 10px;
+  display: inline-block;
+  text-align: center;
+  width: 22.5%;
+  margin: 6px 3px;
+`;
+
+export const PokemonName = styled.p`
+  color: lightcoral;
+  -webkit-text-stroke: 1px red;
+  font-size: 1.5em;
+`;
